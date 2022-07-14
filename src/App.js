f87c06906ad37a84f97fd6c4499d927f67cc0237
apd5392 <adoumbia.pas@gmail.com>
2022-07-14 07:06:40 -0400

    Finished. Cant enter data for password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import Validator from "./components/Validator"
+import Validator from "./components/Validator";
+import { useState } from "react";
 
 const App = () => {
+  const [username, setUsername] = useState("");
 
+  const handleChange = (event) => {
+    event.preventDefault();
+    setUsername(event.target.value);
+  };
+
+  const [password, setPassword] = useState("");
+
+  const handleChange2 = (event) => {
+    event.preventDefault();
+    setPassword(event.target.value);
+  };
+
+  const [passwordC, setPasswordConfrim] = useState("");
+
+  const handleChange3 = (event) => {
+    event.preventDefault();
+    setPasswordConfrim(event.target.value);
+  };
+
+  let [validate, setValidate] = useState("");
+
+  const handleChange4 = (event) => {
+    event.preventDefault();
+    setValidate(event.target.value);
+  };
+
+  const validatePassword = () => {
+    if (password === passwordC) {
+      validate = true;
+      alert(`Welcome`);
+    } else {
+      validate = false;
+      alert("Passwords don't match.");
+    }
+  };
   return (
-    <Validator />
-  )
-}
+    <Validator
+      handleChange={handleChange}
+      handleChange1={handleChange2}
+      handleChange2={handleChange3}
+      handleChange3={handleChange4}
+      validatePassword={validatePassword}
+      username={username}
+      password={password}
+      passwordC={passwordC}
+      validate={validate}
+    />
+  );
+};
 
-export default App
+export default App;
--- src/components/Validator.jsx
+++ src/components/Validator.jsx
@@ -1,29 +1,46 @@
-import '../styles/Validator.css'
+import "../styles/Validator.css";
 
-const Validator = () => {
-  
+const Validator = (props) => {
   return (
     <div className="form">
       <h1>Sign Up</h1>
       <form>
-        <input type="text" placeholder="Username" id="username" />
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          onChange={props.handleChange}
+          value={props.username}
+        />
         <label htmlFor="username">Username</label>
-
-        <input type="password" placeholder="Password" id="password" />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={props.handleChange2}
+          value={props.password}
+        />
         <label htmlFor="password">Password</label>
-
         <input
           type="password"
           placeholder="Confirm password"
-          id="passwordConfirm"
+          id="passwordC"
+          onChange={props.handleChange3}
+          value={props.passwordConfirm}
         />
-        <label htmlFor="passwordConfirm">Confirm password</label>
-
-        <button type="submit">Sign Up</button>
+        <label htmlFor="passwordC">Confirm password</label>
+        <button
+          type="submit"
+          onClick={props.validatePassword}
+          onChange={props.handleChange4}
+          value={props.validate}
+        >
+          Sign Up
+        </button>
         <p>Passwords must match.</p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Validator
+export default Validator;
